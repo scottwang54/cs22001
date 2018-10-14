229d15e4375a5663ec381f27a37c1446b1fddd41
ScottWang <zirenw@uchicago.edu>
2018-10-13 21:39:16 -0500

    edits to community tests

--- warm-up-project/test/test_community.cpp
+++ warm-up-project/test/test_community.cpp
@@ -13,14 +13,36 @@ protected:
 
 // test get_name
 TEST_F(test_community, get_name) {
+	EXPECT_STREQ(community.get_name().c_str(),"legitcommunity");
 }
 
 // test set_name
 TEST_F(test_community, set_name) {
+	EXPECT_FALSE(community.set_name("morethanallowedmorethanallowedmorethanallowedmorethanallowedmorethanallowedmorethanallowedmorethanallowedmorethanallowedmorethanallowed")); // more than 128 (135 here) characters. should fail. 
+	EXPECT_FALSE(community.set_name("community!")); // contains non alphanumeric characters. should fail.
+	EXPECT_FALSE(community.set_name("3community")); // starts with a digit. should fail. 
+	EXPECT_FALSE(community.set_name("")); // empty string. should fail. 
+	EXPECT_TRUE(community.set_name("legitcommunity")); // should pass.
 }
 
 // test add_person
 TEST_F(test_community, add_person) {
+	
+
+	// below we add the new person
+	Person* new_person = new Person();
+	new_person->set_username("csiluser2");
+	new_person->set_firstname("Tim");
+	new_person->set_lastname("Cook");
+	new_person->set_age(55);
+	new_person->set_gender(2);
+	community.add_person(*new_person);
+
+	// below we test if the new person has been successfully added
+	Person retrieved_person = community.get_member("csiluser2");
+	EXPECT_STREQ(retrieved_person.get_username().c_str(),"csiluser2");
+	EXPECT_STREQ(retrieved_person.get_firstname().c_str(),"Tim");
+	EXPECT_STREQ(retrieved_person.get_lastname().c_str(),"Cook");
 }
 
 // test get_all_usernames
@@ -35,6 +57,8 @@ TEST_F(test_community, find_member) {
 
 // test get_member
 TEST_F(test_community, get_member) {
+
+
 }
 
 // test send_msg
