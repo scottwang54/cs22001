eb7b64ae42691a8f9e4a68b785104c931226f08e
ScottWang <zirenw@uchicago.edu>
2018-10-14 17:46:19 -0500

    find members

--- warm-up-project/test/test_community.cpp
+++ warm-up-project/test/test_community.cpp
@@ -32,16 +32,12 @@ TEST_F(test_community, add_person) {
 	// below we add the new person
 	Person* new_person = new Person("csiluser3", "Tim", "Cook", 2, 30, "AwesomePossum");
 	community.add_person(*new_person);
-
 	// below we test if the new person has been successfully added
 	Person retrieved_person = community.get_member("csiluser3");
 	EXPECT_STREQ(retrieved_person.get_username().c_str(),"csiluser3");
 	EXPECT_STREQ(retrieved_person.get_firstname().c_str(),"Tim");
 	EXPECT_STREQ(retrieved_person.get_lastname().c_str(),"Cook");
 	EXPECT_EQ(retrieved_person.get_age(),30);
-
-	// NOT WORKING! 
-
 }
 
 // test get_all_usernames
@@ -52,12 +48,34 @@ TEST_F(test_community, get_all_usernames) {
 
 // test find_member by first name and age range
 TEST_F(test_community, find_member) {
+
+	// below we add the new person
+	Person* new_person = new Person("csiluser4", "Eric", "Smith", 2, 30, "AwesomePossum");
+	community.add_person(*new_person);
+
+	// below we set up containers for testing
+	list<Person> found_list;
+	Person found_person;	
+	
+	// below we test if we can find the member, by firstname
+	found_list = community.find_member("Eric");
+	if (!found_list.empty()) {
+		found_person = found_list.front();
+	}
+	EXPECT_STREQ(found_person.get_firstname().c_str(),"Eric");
+
+	// below test if we can find the member, by agerange
+	found_list = community.find_member(20,30);
+	if (!found_list.empty()) {
+		
+	}
+
+
+
 }
 
 // test get_member
 TEST_F(test_community, get_member) {
-
-
 }
 
 // test send_msg
