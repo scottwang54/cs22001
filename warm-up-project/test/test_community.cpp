8a62e19a093cceea2bbc6a3e8d95e8f0b7be60f4
ScottWang <zirenw@uchicago.edu>
2018-10-14 21:14:13 -0500

    done find_member (firstname) and find_member (age)

--- warm-up-project/test/test_community.cpp
+++ warm-up-project/test/test_community.cpp
@@ -60,32 +60,109 @@ TEST_F(test_community, add_person) {
 TEST_F(test_community, get_all_usernames) {
 }
 
+// Scott's edit!
+// https://stackoverflow.com/questions/16747591/how-to-get-an-element-at-specified-index-from-c-list
+string get_element_in_list(list<string> _list, int _i) {
+	list<string>::iterator it = _list.begin();
+	for (int i = 0; i<_i; i++) {
+		++it;
+	}
+	return *it;
+} 
+
+// Scott's edit!
+// https://www.geeksforgeeks.org/check-if-two-arrays-are-equal-or-not/
+bool lists_are_equal(list<Person> list1, list<Person>list2) {
+	
+	list<string> usernames1;
+	list<string> usernames2;
+
+	// build list of usernames
+	for (list<Person>::iterator it = list1.begin(); it!=list1.end(); it++) {
+		usernames1.push_back((*it).get_username());
+	}
+
+	for (list<Person>::iterator it = list2.begin(); it!=list2.end(); it++) {
+		usernames2.push_back((*it).get_username());
+	}
+
+	int n = usernames1.size();
+	int m = usernames2.size();
+	
+	if (n != m)
+		return false;
+	
+	usernames1.sort();
+	usernames2.sort();
+
+	for (int i = 0; i<n; i++) {
+		if (strcmp(get_element_in_list(usernames1, i).c_str(), 
+			get_element_in_list(usernames2, i).c_str()) != 0)
+			return false;
+		return true;
+	}
+}
+
+// Scott's edit!
 // test find_member by first name and age range
 TEST_F(test_community, find_member) {
 
-	// below we add the new person
-	Person* new_person = new Person("csiluser4", "Eric", "Smith", 2, 30, "AwesomePossum");
-	community.add_person(*new_person);
+	Person* new_person3 = new Person("csiluser3", "Tim", "Cook", 2, 30, "Possum");
+	Person* new_person4 = new Person("csiluser4", "Tim", "Boyer", 2, 55, "Possum");
+	Person* new_person5 = new Person("csiluser5", "Steve", "Jobs", 2, 55, "Possum");
+	Person* new_person6 = new Person("csiluser6", "Rob", "Zimmer", 2, 100, "Possum");
+	Person* new_person7 = new Person("csiluser7", "Tim", "Rogers", 2, 60, "Possum");
+	community.add_person(*new_person3);
+	community.add_person(*new_person4);
+	community.add_person(*new_person5);
+	community.add_person(*new_person6);
+	community.add_person(*new_person7);
 
-	// below we set up containers for testing
-	list<Person> found_list;
 	Person found_person;	
+	list<Person> found_list;
+	list<Person> correct_list;
 	
-	// below we test if we can find the member, by firstname
-	found_list = community.find_member("Eric");
-	if (!found_list.empty()) {
-		found_person = found_list.front(); // NEED WORK: THIS ONLY CHECKS THE FIRST MEMBER
-	}
-	EXPECT_STREQ(found_person.get_firstname().c_str(),"Eric");
+	// Testing firstname
+	found_list = community.find_member("Tim");
+	correct_list.push_back(*new_person3);
+	correct_list.push_back(*new_person4);
+	correct_list.push_back(*new_person7);
+	if (lists_are_equal(found_list,correct_list) == true) 
+		EXPECT_TRUE(1); // if lists are equal, then test will generate true
+	else
+		EXPECT_TRUE(0); // if lists are not equal, then test will generate false
+	correct_list.clear();
 
-	// below test if we can find the member, by agerange
-	found_list = community.find_member(20,30);
-	if (!found_list.empty()) {
-		std::list<Person>::iterator it;
-		for (it = found_list.begin(); it != found_list.end(); it++) {
-			EXPECT_STREQ(it->get_firstname().c_str(),"EricAHH");
-		}
-	}
+	found_list = community.find_member("Rob");
+	correct_list.push_back(*new_person6);
+	if (lists_are_equal(found_list,correct_list) == true) 
+		EXPECT_TRUE(1); 
+	else
+		EXPECT_TRUE(0); 
+	correct_list.clear();
+
+	// Testing age
+	found_list = community.find_member(25,70);
+	correct_list.push_back(*new_person3);
+	correct_list.push_back(*new_person4);
+	correct_list.push_back(*new_person5);
+	correct_list.push_back(*new_person7);
+	if (lists_are_equal(found_list,correct_list) == true) 
+		EXPECT_TRUE(1); 
+	else
+		EXPECT_TRUE(0); 
+	correct_list.clear();
+
+	found_list = community.find_member(50,70);
+	correct_list.push_back(*new_person4);
+	correct_list.push_back(*new_person5);
+	correct_list.push_back(*new_person7);
+	if (lists_are_equal(found_list,correct_list) == true) 
+		EXPECT_TRUE(1); 
+	else
+		EXPECT_TRUE(0); 
+	correct_list.clear();
+
 }
 
 // test get_member
